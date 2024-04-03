c115ba4d5add09d1a129d254f1c7c2ef975e09ce
mandala8 <165963266+mandala8@users.noreply.github.com>
2024-04-04 05:20:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const wrapper document.querySelector(".wrapper");
+
+const question = document.querySelector(".question");
+
+const gif document.querySelector(".gif");
+
+const yesBtn = document.querySelector(".yes-btn");
+
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () {
+
+question.innerHTML = "Aaaaa, I like you too";
+
+gif.src =
+
+"https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; });
+
+noBtn.addEventListener("mouseover", () {
+
+const noBtnRect = noBtn.getBoundingClientRect();
+
+const maxx window.innerWidth noBtnRect.width;
+
+const maxy = window.innerHeight noBtnRect.height;
+
+const randomX = Math.floor(Math.random()* maxx);
+
+const randomyY = Math.floor(Math.random()* maxY);
+
+noBtn.style.left randomX + "px";
+
+});
+
+noBtn.style.top randomy + "px";
+
